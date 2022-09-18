923e8c3d59828cac55ac012a915cee809b1230fa
Syed Muzammil Ali <syedmuzammil67@gmail.com>
2022-09-18 12:25:59 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const CupertinoStoreApp());
+}
+
+
+class CupertinoStoreApp extends StatelessWidget {
+  const CupertinoStoreApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
